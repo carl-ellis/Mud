65722f68423064592b437942ae6d3219296d0199
Carl Ellis <carlc75@gmail.com>
2012-02-10 16:44:49 +0000

    Added the caching methods, ready for the JSON methods

--- storage/repository.rb
+++ storage/repository.rb
@@ -14,9 +14,47 @@
 #   Simple API for getting objects
 #     - GetRoom, GetArea, GetID, etc etc
 #
-#		Simple API for creating objects
+#    Simple API for creating objects
 #     - CreateRoom, CreateArea, FreeID, etc, etc
 #     * IDs should be unique across everything, so free ids should be tracked
 class Repository
+
+	# Cache
+	@cache_size = 0
+	@cache   = []
+
+	# Constructor
+	def initialize(cache_size)
+		@cache, @cache_size = [], cache_size
+	end
+
+	# Gets an item from the cache, or if not there, gets via storage method
+  # Arguments:
+	#   id  = Object id
+  #
+  # Returns:
+	#   obj = Object
+	def get_from_cache(id)
+		
+		# Check cache
+		obj = nil
+		@cache.each{ |o| obj = o if o.id == id }
+
+		# get from storage if not in the cache
+		obj = get_from_storage(id) if obj.nil?
+
+		return obj
+	end
+
+	# Virtual method - Should be overridden my storage class
+	# Gets an object from storage
+  # Arguments:
+	#   id  = Object id
+  #
+  # Returns:
+	#   obj = Object
+	def get_from_storage(id)
+		raise "[FATAL] Storage model must be used"
+	end
 end
 
