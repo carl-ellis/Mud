9c53aea47dffe26623310475854aed9db26aee39
Carl Ellis <carlc75@gmail.com>
2012-04-08 12:30:34 +0100

    Salted the bcrypt library

--- util/password.rb
+++ util/password.rb
@@ -6,12 +6,45 @@ class Password
 
   # Generates a new salt and rehashes the password
   def Password.update(password)
-    hash = BCrypt::Password.create(password).to_s
+		salt = self.salt
+    hash = self.create_hash(password, salt)
+		self.store(hash, salt)
   end
 
   # Checks the password against the stored password
   def Password.check(password, store)
-    hash = BCrypt::Password.new(store)
-    hash == password
+		salt = get_salt(store)
+		hash = get_hash(store)
+    hash = BCrypt::Password.new(hash)
+    hash == "#{password}:#{salt}"
   end
+
+	protected
+
+	# Create a random salt
+	def Password.salt
+		salt = ""
+    64.times { salt << (i = Kernel.rand(62); i += ((i < 10) ? 48 : ((i < 36) ? 55 : 61 ))).chr }
+    salt
+	end
+
+	# Create the hash using a salt
+	def Password.create_hash(password, salt)
+    BCrypt::Password.create("#{password}:#{salt}").to_s
+	end
+
+  # Combine the hash and the salt for storage
+	def Password.store(hash, salt)
+		"#{hash}#{salt}"
+	end
+
+	# Get the hash from the stored string
+	def Password.get_hash(store)
+		store[0..59]
+	end
+
+	# Get the salt from the end of the stored hash
+	def Password.get_salt(store)
+		store[60..124]
+	end
 end
